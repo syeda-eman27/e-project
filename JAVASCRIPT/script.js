f74c606f8a8b849426b54b1c3a8390f78a392756
Maan <eman2409e@aptechsite.net>
2025-03-03 00:50:45 +0500

    add more.....

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,11 +1,12 @@
-AOS.init();
-
 AOS.init({
-    offset: 120,
-    delay: 200, 
-    duration: 1000, 
-    easing: 'ease', 
-    once: true, 
+    offset: 100,
+    delay: 0,
+    duration: 800,
+    easing: 'ease-in-out',
+    once: true,
+    mirror: false,
+    anchorPlacement: 'top-bottom',
+    disable: 'mobile'
 });
 
 let chatContainer = null;
