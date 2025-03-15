490d80309ccfa369a6ac17bbe76874431f832f02
Maan <eman2409e@aptechsite.net>
2025-03-15 23:31:16 +0500

    adddd

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -200,4 +200,26 @@ function showSlider(type){
 }
 
 
-// home page 
+document.addEventListener('DOMContentLoaded', function() {
+    var tinyslider = function() {
+        var el = document.querySelectorAll('.testimonial-slider');
+
+        if (el.length > 0) {
+            var slider = tns({
+                container: '.testimonial-slider',
+                items: 1,
+                axis: "horizontal",
+                controlsContainer: "#testimonial-nav",
+                swipeAngle: false,
+                speed: 700,
+                nav: true,
+                controls: true,
+                autoplay: true,
+                autoplayHoverPause: true,
+                autoplayTimeout: 3500,
+                autoplayButtonOutput: false
+            });
+        }
+    };
+    tinyslider();
+});
