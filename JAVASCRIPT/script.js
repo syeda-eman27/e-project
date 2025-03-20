8afafa45f29bcf9af0f866bfe335b8a254b98a69
Maan <eman2409e@aptechsite.net>
2025-03-20 20:17:16 +0500

    completedd

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -271,3 +271,11 @@ function showSlider(type){
     }, timeAutoNext)
 }
 
+    // // Loader Hide Script
+    // window.addEventListener('load', function() {
+    //     const loading = document.getElementById('loading');
+    //     loading.style.opacity = '0';
+    //     setTimeout(() => {
+    //         loading.style.display = 'none';
+    //     }, 500);
+    // });
